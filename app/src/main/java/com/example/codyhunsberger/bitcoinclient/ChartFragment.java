b88caea6c3ec8eb57d5fa2a70e3e0976990666cc
Cody Hunsberger <codythomashunsberger@gmail.com>
2016-12-11 22:45:59 -0500

    Progress save 12-11-16 22:45

--- app/src/main/java/com/example/codyhunsberger/bitcoinclient/ChartFragment.java
+++ app/src/main/java/com/example/codyhunsberger/bitcoinclient/ChartFragment.java
@@ -1,6 +1,7 @@
 package com.example.codyhunsberger.bitcoinclient;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.os.CountDownTimer;
@@ -8,17 +9,18 @@ import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.RadioButton;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ChartFragment extends Fragment {
 	String chartUrl, chartRefreshTimerText;
 	WebView cwv;
+	boolean loadingFinished, redirect;
 	int secondsUntilFinished;
 	CountDownTimer cdt;
 	TextView timerText;
@@ -39,61 +41,48 @@ public class ChartFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_chart, container, false);
 		timerText = (TextView) v.findViewById(R.id.chartRefreshTimerTextview);
 		RadioGroup rg = (RadioGroup) v.findViewById(R.id.chart_radio_group);
-
 		cwv = (WebView) v.findViewById(R.id.chartWebView);
 		cwv.getSettings().setUseWideViewPort(true);
 		cwv.getSettings().setLoadWithOverviewMode(true);
 
+
+
+		chartUrl = "https://chart.yahoo.com/z?s=BTCUSD=X&t=1d";
+
+		displayUrl(chartUrl);
+
+		rg.setOnCheckedChangeListener(
+			new RadioGroup.OnCheckedChangeListener() {
+				@Override
+				public void onCheckedChanged(RadioGroup group, int checkedId) {
+					switch (checkedId) {
+						case(R.id.radioButton1d):
+							chartUrl = "https://chart.yahoo.com/z?s=BTCUSD=X&t=1d";
+							break;
+						case(R.id.radioButton5d):
+							chartUrl = "https://chart.yahoo.com/z?s=BTCUSD=X&t=5d";
+							break;
+						default:
+							chartUrl = "https://chart.yahoo.com/z?s=BTCUSD=X&t=1Y";
+							Toast.makeText(getActivity(), "Error in RadioGroup onCheckedChanged " +
+												   "listener. ID received does not match either button.",
+										   Toast.LENGTH_SHORT).show();
+						// I'm fairly certain this can't happen but you can't be too sure
+					}
+					displayUrl(chartUrl);
+				}
+		});
 		return v;
 	}
 
-	public void onCheckedChanged(RadioGroup group, int checkedId) {
-		// find which radio button is selected
-		if(checkedId == R.id.radioButton1d) {
-			chartUrl = "https://chart.yahoo.com/z?s=BTCUSD=X&t=1d";
-		}
-		else if(checkedId == R.id.radioButton5d) {
-
-		}
-		else {
-			Toast.makeText(getActivity(), "Error in radio button click listener method",
-						   Toast.LENGTH_SHORT).show();
-		}
-	}
-
-	public void onRadioButtonClicked(View view) {
-		boolean checked = ((RadioButton) view).isChecked();
-
-		// Check which radio button was clicked
-		switch(view.getId()) {
-			case R.id.radioButton1d:
-				if(checked) {
-					chartUrl = "https://chart.yahoo.com/z?s=BTCUSD=X&t=1d";
-				}
-				break;
-			case R.id.radioButton5d:
-				if(checked) {
-					chartUrl = "https://chart.yahoo.com/z?s=BTCUSD=X&t=5d";
-				}
-				break;
-		}
-
-		if(!isAirplaneModeOn(getActivity())) {
-			loadUrl(chartUrl);
-			newTimer();
-		}
-
-		else{
-			Toast.makeText(getActivity(), "Unable to load, airplane mode is enabled.", Toast
-					.LENGTH_SHORT).show();
-		}
-	}
-
+	// Reloads the chart every minute. The prompt suggested 15s but that seems unnecessary.
+	// Chart source does not update anywhere close to this frequency so it doesn't do too much, but
+	// it's in the requirements.
 	public void newTimer() {
-		cdt = new CountDownTimer(15000, 1000) {
+		cdt = new CountDownTimer(60000, 1000) {
 			public void onFinish() {
-				loadUrl(chartUrl);
-				newTimer();
+				displayUrl(chartUrl);
+				start();
 			}
 
 			public void onTick(long millisUntilFinished) {
@@ -101,27 +90,33 @@ public class ChartFragment extends Fragment {
 				chartRefreshTimerText = getResources().getString(
 						R.string.chart_refresh_timer_text, secondsUntilFinished);
 				timerText.setText(chartRefreshTimerText);
+				// TODO progress bar for reload?
 			}
 		}.start();
-
-		// Reloads the chart every 15s
-		// Chart source does not update anywhere close to this frequency so it doesn't do
-		// too much
 	}
 
-	public void loadUrl(String chartUrl) {
-		Toast.makeText(getActivity(), "Loading...", Toast.LENGTH_SHORT).show();
-		cwv.loadUrl(chartUrl);
+	// Load a fresh chart URL in the webview
+	public void displayUrl(String chartUrl) {
+		if(!isAirplaneModeOn(getActivity())) {
+			Toast.makeText(getActivity(), "Loading...", Toast.LENGTH_SHORT).show();
+			// It's bad design when the Toast outlasts the load time by a factor of 2
+			cwv.loadUrl(chartUrl);
+			newTimer();
+		}
+		else{
+			Toast.makeText(getActivity(), "Unable to load, airplane mode is enabled.", Toast
+					.LENGTH_SHORT).show();
+		}
+
 	}
 
+	// Converts the long milliseconds from CountDownTimer to an integer number of seconds
 	public int safeLongToInt(long l) {
 		if(l < Integer.MIN_VALUE || l > Integer.MAX_VALUE) {
 			throw new IllegalArgumentException(l +
-													   " cannot be cast to int without changing its value.");
+					   	" cannot be cast to int without changing its value.");
 		}
 
 		return (int) l;
 	}
 }
-
-// TODO implement periodic updates (15s)
--- app/src/main/java/com/example/codyhunsberger/bitcoinclient/MainActivity.java
+++ app/src/main/java/com/example/codyhunsberger/bitcoinclient/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends Activity implements ListFragment.ListSelection
                     .commit();
         }
     }
-    // note Still working on implementing this
+    // FIX Still working on implementing this
 
     public void getTicker() {
         urlToJsonString = new UrlToJsonString(this);
